9b749dcea9c34c3d161954a4aaaa53fbe9e2fad4
xosen <gameview@163.com>
2014-07-13 10:53:47 +0800

    删除安卓冗余文件

--- game/proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- E:/WareCocos2DX/cocos2dx/support/ccUtils.cpp \
- E:/WareCocos2DX/cocos2dx/support/ccUtils.h
-
-E:/WareCocos2DX/cocos2dx/support/ccUtils.h:
